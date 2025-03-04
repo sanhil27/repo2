a2f854f6becbe80842567209f06bb1c612c3380f
Sanhil Rai <sanhil.rai@viamagus.com>
2025-03-04 12:14:57 +0530

    BREAKING CHANGE: change endpoint and logic

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+    const a = 5;
+    const b = 6;
+    res.send(`${a} + ${b} = ${a + b}`);
 });
-
-app.get('test1', (req,res) => {
-    res.send('working');
-})
-
 
 app.listen(2000, () => {
     console.log('express app listening on port 2000');
