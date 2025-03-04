e350cd83b77c31c35f4c2ac1e2c4fa683aa753ca
Sanhil Rai <sanhil.rai@viamagus.com>
2025-03-04 12:26:15 +0530

    feat: change logic in default api BREAKING CHANGE: this upate modifies api response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     const a = 5;
     const b = 6;
-    res.send(`${a} + ${b} = ${a + b}`);
+    res.send(`${a} + ${b} = ${a + b}, ${a*b}`);
 });
 
 app.listen(2000, () => {
