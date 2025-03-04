e94e7069d9b20730a6f1f389c04edea2e9c453be
Sanhil Rai <sanhil.rai@viamagus.com>
2025-03-04 12:10:16 +0530

    fix: fixing test1 endpoint

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('test1', (req,res) => {
+    res.send('working');
+})
+
 
 app.listen(2000, () => {
     console.log('express app listening on port 2000');
